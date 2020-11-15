44ec69f28e5f88497c756abbf8b8005dc0cb4f0a
Justin <justinchau31@gmail.com>
2020-11-14 22:11:37 -0800

    Close to completeing the Mobile Responsiveness

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+let questions = document.getElementsByClassName("question");
+
+
+for (let i = 0; i < questions.length; ++i) {
+    questions[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        
+        let img = this.querySelector('.dropdown')
+        
+        img.classList.toggle("reverse")
+
+        let panel = this.nextElementSibling;
+        
+        if (panel.style.display === "block") {
+            panel.style.display = "none"
+        } else {
+            panel.style.display = "block"
+            
+        }
+    })
+}
+
+
